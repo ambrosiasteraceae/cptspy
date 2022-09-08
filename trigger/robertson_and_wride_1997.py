5437dd26a60638b335d4e224d2476f5ed5ccf9aa
Ambrozie <dseculin@gmail.com>
2022-09-08 17:12:22 +0400

    #Work on robertson_and_wride_1997 #2 #Needs testing. FoS gives absurd values

--- trigger/robertson_and_wride_1997.py
+++ trigger/robertson_and_wride_1997.py
@@ -2,6 +2,7 @@ import numpy as np
 import liquepy as lq
 from fig import timed
 
+
 def calc_qt(qc, ar, u2):
     """
 
@@ -113,6 +114,14 @@ def calc_i_c(big_q, big_f):
         big_q = 1
     return ((3.47 - np.log10(big_q)) ** 2 + (1.22 + np.log10(big_f)) ** 2) ** 0.5
 
+
+def calc_csr(sigma_veff, sigma_v, pga, rd):
+    """
+    Cyclic stress ratio from CPT
+    """
+    return 0.65 * (sigma_v / sigma_veff) * rd * pga
+
+
 @timed.timed
 def calc_dependent_variables(sigma_v, sigma_veff, f_s, p_a, q_t):
     """
@@ -133,26 +142,43 @@ def calc_dependent_variables(sigma_v, sigma_veff, f_s, p_a, q_t):
             big_q[dd] = calc_big_q_values(q_t[dd], sigma_v[dd], sigma_veff[dd], p_a, n_val=n_val)
             big_f[dd] = calc_big_f_values(f_s[dd], q_t[dd], sigma_v[dd])
             i_c[dd] = calc_i_c(big_q[dd], big_f[dd])
-            current_n = max(0.5, 0.381 * i_c[dd] + 0.05 * (sigma_veff[dd] / p_a) - 0.15)
-            #print(n_val, 0.381 * i_c[dd] + 0.05 * (sigma_veff[dd] / p_a) - 0.15)
-            if n_val - current_n < 0.01:
+            n_val_current = max(0.5, 0.381 * i_c[dd] + 0.05 * (sigma_veff[dd] / p_a) - 0.15)
+            # print(n_val, 0.381 * i_c[dd] + 0.05 * (sigma_veff[dd] / p_a) - 0.15)
+            if n_val - n_val_current < 0.01:
                 break
             else:
-                n_val = current_n
-        #print("exit 2nd loop,",n_val, f'{current_n:.2f}', n_val - current_n)
+                n_val = n_val_current
+        # print("exit 2nd loop,",n_val, f'{n_val_current:.2f}', n_val - n_val_current)
     return big_q, big_f, i_c
 
-def k_sigma(i_c,big_f):
+
+def calc_k_sigma(i_c, big_f):
     """
     Returns k_sigma
     i_c values higher than 2.6 and lower than 1.64 have 1
     """
-    cond_1 = (lf.i_c <= 1.64) | (lf.i_c > 2.6)
-    cond_2 = ((lf.i_c > 1.64) & (lf.i_c <= 2.36)) & (lf.big_f < 0.5)
-    where__ = np.where(cond_2, 1,
-                       5.581 * lf.i_c ** 3 - 0.403 * lf.i_c ** 4 - 21.63 * lf.i_c ** 2 + 33.75 * lf.i_c - 17.88)
-    np.where(cond_1, 1, where__)
+    cond_1 = (i_c <= 1.64) | (i_c > 2.6)
+    cond_2 = ((i_c > 1.64) & (i_c <= 2.36)) & (big_f < 0.5)
+    k_sigma_cond_2 = np.where(cond_2, 1,
+                       5.581 * i_c ** 3 - 0.403 * i_c ** 4 - 21.63 * i_c ** 2 + 33.75 * i_c - 17.88)
 
+    k_sigma = np.where(cond_1, 1, k_sigma_cond_2)
+
+    return k_sigma
+
+
+    
+def calc_crr_m7p5(big_q_cs):
+    
+    crr = np.where(big_q_cs < 50,
+                   0.833 * big_q_cs / 1000 + 0.05,
+                   93 * big_q_cs ** 3 / 1000 + 0.08)
+    return crr
+def calc_msf(m_w):
+    """
+    Returns magnitude scaling factor
+    """
+    return 174 / (m_w ** 2.56)
 
 class RobertsonWride1997CPT(object):
 
@@ -234,22 +260,31 @@ class RobertsonWride1997CPT(object):
         self.sigma_veff = calc_sigma_veff(self.sigma_v, self.pore_pressure)
         if self.sigma_veff[0] == 0.0:
             self.sigma_veff[0] = 1.0e-10
-        self.rd = calc_rd(self.depth)
+
 
         self.big_q, self.big_f, self.i_c = calc_dependent_variables(self.sigma_v, self.sigma_veff, self.cpt.f_s,
-                                                                  self.p_a, self.q_t)
-        #print(self.big_f[0])
-        #self.k_sigma = calc_k_sigma()
-        #self.big_q_cs = calc_big_q_cs()
+                                                                    self.p_a, self.q_t)
+
+        self.k_sigma = calc_k_sigma(self.i_c, self.big_f)
+        self.big_q_cs = self.big_q * self.k_sigma
+        self.crr_m7p5 = calc_crr_m7p5(self.big_q_cs)
+        self.rd = calc_rd(self.depth)
+        self.csr = calc_csr(self.sigma_veff, self.sigma_v, pga, self.rd)
+        self.msf = calc_msf(self.m_w)
+
+
+        fs_unlimited = self.csr / self.crr_m7p5
+        print(fs_unlimited)
+        fos = np.where(fs_unlimited > 2, 2, fs_unlimited)
+        self.factor_of_safety = np.where(self.i_c <= self.i_c_limit, fos, 2.25)
+        # print(self.big_f[0])
+        # self.k_sigma = calc_k_sigma()
+        # self.big_q_cs = calc_big_q_cs()
+
 
 cpt = lq.field.load_mpa_cpt_file("CPT_H15c.csv", delimiter=";")
-bi2014 = lq.trigger.run_bi2014(cpt, pga = 0.33,m_w = 6)
+#bi2014 = lq.trigger.run_bi2014(cpt, pga=0.33, m_w=6)
 lf = RobertsonWride1997CPT(cpt)
+print(lf.big_q_cs)
+print(lf.factor_of_safety)
 
-import matplotlib.pyplot as plt
-
-plt.plot(lf.i_c,lf.depth, label = 'R&W')
-plt.plot(bi2014.i_c,lf.depth, label = 'B&I', color = 'r')
-plt.legend()
-plt.show()
-print(bi2014.i_c)
